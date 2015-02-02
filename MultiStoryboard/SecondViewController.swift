0524a21e9ad5d6aa4a9439c67e8eacb1bea594b9
adow <reynoldqin@gmail.com>
2015-02-02 23:06:47 +0800

    Initial Commit

---
+++ MultiStoryboard/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MultiStoryboard
+//
+//  Created by 秦 道平 on 15/2/2.
+//  Copyright (c) 2015年 秦 道平. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
